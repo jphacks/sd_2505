a3c5a1554effe482fc8b8dd706471947ac55c70e
O-Aoi <tkogikogi@gmail.com>
2025-10-18 18:57:19 +0900

    backend

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ def split_book_text():
     
     # 1. フロントエンドから送信されたJSONから 'text' を取り出す
     data = request.json
-    full_text = data.get('text')
+    full_text = data.get('description')
 
     if not full_text:
         return jsonify({"error": "本文テキスト ('text') がありません"}), 400
