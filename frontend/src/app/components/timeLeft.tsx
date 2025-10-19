10582785673a022d46a68858ac964371692952c0
O-Aoi <tkogikogi@gmail.com>
2025-10-19 12:33:58 +0900

    update top page header

--- frontend/src/app/components/timeLeft.tsx
+++ frontend/src/app/components/timeLeft.tsx
@@ -1,7 +1,7 @@
 export function timeIcon(){
     return (
-        <span className="absolute top-0 right-0 inline-flex items-center justify-center rounded-full bg-orange-100 px-2 py-1 text-xs font-medium text-gray-600 inset-ring inset-ring-gray-500/10">
-            5 min
+        <span className="absolute top-0 right-0 inline-flex items-center justify-center rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-gray-600 inset-ring inset-ring-gray-500/10">
+            0 min
         </span>
     )
 }
